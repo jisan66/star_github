4b4867c01a584d0b340729a4a6c8325067097239
Zesan Saha <41072153+jisan66@users.noreply.github.com>
2025-02-09 16:14:04 +0600

    release key generated and added

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:star_github/presentation/details_screen/details_screen.dart';
 import 'package:star_github/presentation/home_screen/bloc/home_screen_bloc.dart';
 import 'package:star_github/presentation/home_screen/homescreen_view.dart';
 import 'package:star_github/utils/themes/custom_theme.dart';
@@ -14,12 +13,11 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
 
-  await FastCachedImageConfig.init(clearCacheAfter: const Duration(days: 7));
+  await FastCachedImageConfig.init(clearCacheAfter: const Duration(minutes: 1));
 
   var dir = await getApplicationDocumentsDirectory();
   await Hive.initFlutter(dir.path);
   Hive.registerAdapter(ItemAdapter());
-  Hive.registerAdapter(LicenseAdapter());
   Hive.registerAdapter(OwnerAdapter());
   Hive.registerAdapter(AppDataAdapter());
   await Hive.openBox<Item>('items');
